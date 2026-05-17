522ce18730210abdc4596ba761612878650dba97
ooliveira-ops <filipecordeirooliveira838@gmail.com>
2026-05-17 15:05:45 -0300

    feat:create Person and PersonAddress models

---
+++ src/Api/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Api.Models
+{
+	public class Person
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime DateOfBirth { get; set; }
+
+		//Relacionamento com PersonAddress
+		public int AddressId { get; set; }
+		public PersonAddress Address { get; set; }
+	}
+}
---
+++ src/Api/Models/PersonAddress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Api.Models
+{
+	public class PersonAddress
+	{
+		public int Id { get; set; }
+		public string Street { get; set; }
+		public 
+	}	
+}
