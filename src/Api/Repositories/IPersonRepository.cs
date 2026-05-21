62ff7d80cd81c684b993627d79bca40f99a3dc6e
ooliveira-ops <filipecordeirooliveira838@gmail.com>
2026-05-21 01:29:51 -0300

    feat: implement repository pattern with PersonRepository

---
+++ src/Api/Repositories/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using person_management_api.Models;
+
+namespace person_management_api.Repositories
+{
+	public interface IPersonRepository									   // Interface: define o "o que" fazer
+																		   	
+	{
+		Task<Person> GetByIdAsync(int id);
+		Task<List<Person>> GetAllAsync(int pageNumber = 1, int pageSize = 10);
+		Task<List<Person>> SearchAsync(string searchTerm, int pageNumber = 1, int pageSize = 10);
+		Task CreateAsync(Person person);
+		Task UpdateAsync(Person person);
+		Task DeleteAsync(int id);
+	}
+}
+
